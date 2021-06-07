2c8ab9fee0910f4589fd394cbe895459494f493e
Sergey Stavichenko <sergey.stavichenko@gmail.com>
2021-06-07 15:57:54 +0300

    execution time test

--- tests.php
+++ tests.php
@@ -81,14 +81,54 @@ class MergeTest extends TestCase
     ];
 
     /** @dataProvider dataProvider */
-    public function testMerge($input, $output)
-    {
+    public function testMerge($input, $output) {
         $actualOutput = merge($input);
         $this->assertSame($actualOutput, $output);
     }
 
-    public function dataProvider()
-    {
+    public function testBigVolume() {
+        $input = $output = [];
+        $startDate = new \DateTime('2021-05-05 6:00');
+        $endDate = new \DateTime('2021-05-05 6:45');
+
+        for ($i = 0; $i < 100; $i++) {
+            $startDate->modify('+1 day');
+            $endDate->modify('+1 day');
+
+            for ($j = 0; $j < 12; $j += 2) {
+                $shouldIntersect = ($i + $j) % 7 === 0;
+                $pair = [];
+
+                for ($k = 0; $k < 2; $k++) {
+                    $start = (clone $startDate)->modify(($j + $k) . ' hour');
+                    $end = (clone $endDate)->modify(($j + $k) . ' hour');
+
+                    if ($k === 0 && $shouldIntersect) {
+                        $end->modify('+30 mins');
+                    }
+
+                    $period = [$start->format('Y-m-d H:i:s'), $end->format('Y-m-d H:i:s')];
+                    $pair[] = $period;
+                    $input[] = $period;
+                }
+
+                if ($shouldIntersect) {
+                    $pair = [[$pair[0][0], $pair[1][1]]];
+                }
+
+                array_push($output, ...$pair);
+            }
+        }
+
+        $time = new \DateTime();
+        $actualOutput = merge($input);
+        $evalTime = (new \DateTime())->getTimestamp() - $time->getTimestamp();
+        print($evalTime);
+        $this->assertSame($actualOutput, $output);
+        $this->assertLessThan(2, $evalTime);
+    }
+
+    public function dataProvider() {
         return static::CASES;
     }
 }
